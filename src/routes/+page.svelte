30a8a74f8affc6ec020f230b0027f1cf13ed88e9
lamorak13 <j.e.mayerhofer@gmail.com>
2022-12-06 12:17:45 +0100

    Chore: add Flowbite UI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<Alert>
+		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
+	</Alert>
+</div>
