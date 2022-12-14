4471a6dd2ee50113fe28ee8c3653ea392bd10839
Dorian Zedler <dorian@itsblue.de>
2022-12-14 22:31:02 +0100

    Test: change condition

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import { browser, dev } from '$app/environment';
 import Cookies from 'cookies-ts';
 
 export const API_URL =
-	!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+	process.env.NODE_ENV && process.env.NODE_ENV === 'development'
 		? 'http://127.0.0.1:8000/v1'
 		: 'https://api.speedclimbing.org/v1';
 
