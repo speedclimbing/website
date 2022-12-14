3d25cde3ff1e011a965cd23154f44c8f3f37d895
Dorian Zedler <dorian@itsblue.de>
2022-12-14 22:19:55 +0100

    Fix: wrong URL in production

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,10 @@
 import { browser, dev } from '$app/environment';
 import Cookies from 'cookies-ts';
 
-export const API_URL = dev ? 'http://127.0.0.1:8000/v1' : 'https://api.speedclimbing.org/v1';
+export const API_URL =
+	!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
+		? 'http://127.0.0.1:8000/v1'
+		: 'https://api.speedclimbing.org/v1';
 
 const cookies = new Cookies();
 
