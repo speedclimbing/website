7b68c0bf4bc1d9827eb9513690645d41ac8a6940
Dorian Zedler <dorian@itsblue.de>
2023-01-11 12:34:57 +0100

    Chore: unify record cards

---
+++ src/compnonents/shared/PersonCard.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import type { Action } from 'svelte/types/runtime/action';
+
+	export let image: string;
+	export let color: string = '#dd972a';
+	export let name: string = 'Name';
+	export let subtitle: string = 'Subtitle';
+	export let time: string = '5.00';
+
+	const cardColor: Action<HTMLElement, undefined> = (node) => {
+		node.style.setProperty(`--cardColor`, color);
+
+		return {
+			update() {
+				node.style.setProperty(`--cardColor`, color);
+			}
+		};
+	};
+</script>
+
+<div
+	class="w-[300px] md:w-[450px] lg:w-[400px] h-[250px] p-5 rounded-md background-gradient relative flex flex-col-reverse"
+	use:cardColor
+>
+	<img src={image} alt={name} class="absolute w-full top-[-50px] left-[-50px]" />
+	<div class="relative p-5 rounded-sm bg-white/[0.85]">
+		<p class="dark:text-black">{name}</p>
+		<p class="text-color text-sm font-normal">{subtitle}</p>
+		<p
+			class="relative xl:absolute xl:top-[50%] xl:translate-x-0 xl:translate-y-[-50%] xl:right-5 font-semibold text-2xl text-color"
+		>
+			{time}
+		</p>
+	</div>
+</div>
+
+<style>
+	.background-gradient {
+		background: linear-gradient(0deg, rgba(55, 65, 81, 0.05), rgba(55, 65, 81, 0.05)),
+			linear-gradient(180deg, rgba(218, 60, 43, 0.03) 47.48%, var(--cardColor) 100%);
+		box-shadow: 0px 20px 50px rgba(17, 24, 39, 0.1);
+	}
+
+	.text-color {
+		color: var(--cardColor);
+	}
+</style>
--- src/routes/WorldRecords.svelte
+++ src/routes/WorldRecords.svelte
@@ -1,13 +1,24 @@
 <script>
-	import RecordCardFemale from '../compnonents/home/RecordCardFemale.svelte';
-	import RecordCardMale from '../compnonents/home/RecordCardMale.svelte';
+	import PersonCard from '../compnonents/shared/PersonCard.svelte';
 </script>
 
 <section id="world-records" class="py-[100px] px-[5%] lg:px-[20%]">
 	<h2 class="text-4xl font-bold text-center">Current World Records</h2>
 	<div id="line" class="bg-yellow rounded-sm w-[100px] h-1 mt-5 mb-10 mx-auto" />
 	<div class="flex flex-col gap-20 mx-auto justify-center mt-20 items-center lg:flex-row lg:gap-10">
-		<RecordCardFemale />
-		<RecordCardMale />
+		<PersonCard
+			image="/images/aleksandra-miroslaw-transparent.png"
+			name="Aleksandra Miroslaw (POL)"
+			time="6,53"
+			subtitle="27. May 2022"
+			color="#dd972a"
+		/>
+		<PersonCard
+			image="/images/kiromal-katibin-transparent.png"
+			name="Kiromal Katibin (INA)"
+			time="5,00"
+			subtitle="08. July 2022"
+			color="#da3c2b"
+		/>
 	</div>
 </section>
