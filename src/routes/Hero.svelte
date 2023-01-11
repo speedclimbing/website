36e01b40f32d5c3c59c08c58bdd2e8f6d2c1408c
lamorak13 <j.e.mayerhofer@gmail.com>
2023-01-11 12:58:25 +0100

    Fix: equalize padding on all sections

--- src/routes/Hero.svelte
+++ src/routes/Hero.svelte
@@ -4,18 +4,21 @@
 	import Line from '../compnonents/shared/Line.svelte';
 </script>
 
-<section id="hero" class="hero-image py-[150px]">
+<section id="hero" class="hero-image pb-[150px] pt-[230px]">
 	<h1 class="font-Raleway text-[36px] sm:text-[56px] lg:text-[64px] leading-none mb-[30px]">
 		<span class="text-white font-light">Welcome to</span><br />
 		<span class="font-bold  text-black">Speedclimbing</span>
 	</h1>
 	<Line length="200px" />
-	<p class="mt-[40px] mb-[30px] font-semibold">
+	<p class="mt-[40px] mb-[30px] font-semibold text-white">
 		The ultimate race to the top. Full of emotions,<br />adrenalin and sportsmanship. Take a look
 		for yourself!
 	</p>
 	<div class="flex gap-3 flex-col sm:flex-row">
 		<PrimaryButton text="Latest Tournaments" />
-		<SecondaryButton text="Top Climbers" />
+		<button
+			class={`py-3 w-[200px] border-2 text-white border-white bg-white/10 rounded-[3px] font-semibold`}
+			>Top Climbers</button
+		>
 	</div>
 </section>
--- src/routes/WorldRecords.svelte
+++ src/routes/WorldRecords.svelte
@@ -6,7 +6,7 @@
 <section id="world-records" class="py-[100px]">
 	<h2 class="text-4xl font-bold text-center">Current World Records</h2>
 	<div id="line" class="bg-yellow rounded-sm w-[100px] h-1 mt-5 mb-10 mx-auto" />
-	<div class="flex flex-col gap-20 mx-auto justify-around mt-20 items-center lg:flex-row lg:gap-10">
+	<div class="flex flex-col gap-20 mx-auto justify-center mt-20 items-center lg:flex-row lg:gap-10">
 		<RecordCardFemale />
 		<RecordCardMale />
 	</div>
