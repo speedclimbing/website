98ff07639d6ffda5bf91d78cd89c545d0cfe0746
Dorian Zedler <dorian@itsblue.de>
2023-01-25 13:50:26 +0100

    Feat: show a spinner while navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,17 @@
 	import Header from '../compnonents/shared/Header.svelte';
 	import Footer from '../compnonents/shared/Footer.svelte';
 	import '../app.postcss';
+	import { navigating } from '$app/stores';
+	import { Spinner } from 'flowbite-svelte';
 </script>
 
 <Header />
-<main>
+<main class={$navigating ? 'blur-sm' : ''}>
 	<slot />
 </main>
+
+{#if $navigating}
+	<Spinner class="fixed top-[50%] left-[50%]" />
+{/if}
+
 <Footer />
