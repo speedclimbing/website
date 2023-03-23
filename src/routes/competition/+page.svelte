7a1f91cdbb4a465a7a865917da6c8aa35fb751db
lamorak13 <j.e.mayerhofer@gmail.com>
2023-03-23 09:55:23 +0100

    Chore: Refactoring code

--- src/routes/competition/+page.svelte
+++ src/routes/competition/+page.svelte
@@ -2,10 +2,13 @@
 	import EventCard from '../../compnonents/home/EventCard.svelte';
 	import type { Competition } from 'src/types/Competition';
 	import type { League } from 'src/types/League';
-	import { Input, Select } from 'flowbite-svelte';
+	import { Input } from 'flowbite-svelte';
 	import type { PageData } from './$types';
+	import { _loadCompetitions, _loadLeagues } from './+page';
 
 	export let data: PageData;
+	let competitions: Competition[] = data.competitions;
+	let leagues: League[] = data.leagues;
 	let name: string = '';
 	let nation: string = '';
 	const currentYear: number = new Date().getFullYear();
@@ -13,45 +16,13 @@
 	let leagueId: string = '';
 
 	async function handleSearch(year: number, name: string, nation: string, leagueId: string) {
-		const competitionResponse = await data.fetch(
-			'https://api.speedclimbing.org/v1/competition?' +
-				new URLSearchParams({
-					from: new Date(year, 0, 1).toISOString().substring(0, 10),
-					to: new Date(year, 11, 31).toISOString().substring(0, 10),
-					name: name,
-					nation: nation,
-					league: leagueId
-				})
-		);
-		const competitionsData: Competition[] = await competitionResponse.json();
-
-		competitionsData.forEach((competition: Competition) => {
-			competition.from = new Date(competition.from);
-			competition.to = new Date(competition.to);
-		});
-
-		const leaugeResponse = await data.fetch(
-			'https://api.speedclimbing.org/v1/league?' +
-				new URLSearchParams({
-					year: year.toString()
-				})
-		);
-		const leaguesData: League[] = await leaugeResponse.json();
-
-		data = { ...data, competitions: competitionsData, leagues: leaguesData };
+		competitions = await _loadCompetitions(year, name, nation, leagueId);
+		leagues = await _loadLeagues(year);
 	}
 
 	$: {
 		handleSearch(year, name, nation, leagueId);
 	}
-
-	let nationSelect = data.nations.map((n) => {
-		return { value: n.id, name: n.code };
-	});
-	nationSelect.sort((a, b) => {
-		return a.name < b.name ? -1 : 1;
-	});
-	nationSelect.unshift({ value: '#', name: 'All nations' });
 
 	let timer: NodeJS.Timeout;
 	const debounce = (newValue: string) => {
@@ -86,15 +57,20 @@
 		</select>
 		<select bind:value={leagueId}>
 			<option value="">All Leagues</option>
-			{#each data.leagues as l}
+			{#each leagues as l}
 				<option value={l.id}>{l.name}</option>
 			{/each}
 		</select>
 
-		<Select items={nationSelect} bind:value={nation} placeholder="Select Nation" />
+		<select bind:value={nation}>
+			<option value="">All nations</option>
+			{#each data.nations as n}
+				<option value={n.id}>{n.code}</option>
+			{/each}
+		</select>
 	</div>
 	<div class="grid grid-cols-1 lg:grid-cols-2 gap-5 my-10">
-		{#each data.competitions as competition, index (index)}
+		{#each competitions as competition, index (index)}
 			<EventCard {competition} />
 		{/each}
 	</div>
--- src/routes/competition/+page.ts
+++ src/routes/competition/+page.ts
@@ -3,21 +3,59 @@ import type { Competition } from 'src/types/Competition';
 import type { Nation } from 'src/types/Nation';
 import type { League } from 'src/types/League';
 
-export const load: Load = async ({ fetch }) => {
-	const competitonResponse = await fetch('https://api.speedclimbing.org/v1/competition');
-	const competitions: Competition[] = await competitonResponse.json();
+export const load: Load = async ({ fetch, url }) => {
+	const year = new Date().getFullYear() - 1;
+	const competitions = await _loadCompetitions(year, '', '', '');
+	const nations = await _loadNations();
+	const leagues = await _loadLeagues(year);
+
+	return { competitions, fetch, nations, leagues, url };
+};
+
+export async function _loadCompetitions(
+	year: number,
+	name: string,
+	nation: string,
+	leagueId: string
+): Promise<Competition[]> {
+	const response = await fetch(
+		'https://api.speedclimbing.org/v1/competition?' +
+			new URLSearchParams({
+				from: new Date(year, 0, 1).toISOString().substring(0, 10),
+				to: new Date(year, 11, 31).toISOString().substring(0, 10),
+				name: name,
+				nation: nation,
+				league: leagueId
+			})
+	);
+	const competitions: Competition[] = await response.json();
 
 	competitions.forEach((competition: Competition) => {
 		competition.from = new Date(competition.from);
 		competition.to = new Date(competition.to);
 	});
+	return competitions;
+}
 
-	const nationResponse = await fetch('https://api.speedclimbing.org/v1/nation');
-	const nations: Nation[] = await nationResponse.json();
+export async function _loadLeagues(year: number): Promise<League[]> {
+	const response = await fetch(
+		'https://api.speedclimbing.org/v1/league?' +
+			new URLSearchParams({
+				year: year.toString()
+			})
+	);
+	const leagues: League[] = await response.json();
 
-	let year: number = new Date().getFullYear();
-	const leagueResonse = await fetch(`https://api.speedclimbing.org/v1/league?year=${year - 1}`);
-	const leagues: League[] = await leagueResonse.json();
+	return leagues;
+}
 
-	return { competitions, fetch, nations, leagues };
-};
+export async function _loadNations(): Promise<Nation[]> {
+	const response = await fetch('https://api.speedclimbing.org/v1/nation');
+	const nations: Nation[] = await response.json();
+
+	nations.sort((a, b) => {
+		return a.name < b.name ? 1 : -1;
+	});
+
+	return nations;
+}
