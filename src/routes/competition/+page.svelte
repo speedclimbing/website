7ed39f6e5fb8809c2a4c2a9eea03c3a6a55a107a
lamorak13 <j.e.mayerhofer@gmail.com>
2023-03-24 08:52:03 +0100

    Feat: implement FullCalendar functionality

--- src/routes/competition/+page.svelte
+++ src/routes/competition/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-	import EventCard from '../../compnonents/home/EventCard.svelte';
 	import type { Competition } from 'src/types/Competition';
 	import type { League } from 'src/types/League';
-	import { Input } from 'flowbite-svelte';
 	import type { PageData } from './$types';
+	import EventCard from '../../compnonents/home/EventCard.svelte';
+	import { Input } from 'flowbite-svelte';
 	import { _loadCompetitions, _loadLeagues } from './+page';
+	import { Calendar } from 'fullcalendar';
+	import dayGridPlugin from '@fullcalendar/daygrid';
+	import { onMount } from 'svelte';
 
 	export let data: PageData;
 	let competitions: Competition[] = data.competitions;
 	let leagues: League[] = data.leagues;
 	let name: string = '';
 	let nation: string = '';
-	const currentYear: number = new Date().getFullYear();
-	let year: number = currentYear;
 	let leagueId: string = '';
+	let viewCalendar: boolean = false;
+	const currentDate: Date = new Date();
+	let year: number = currentDate.getFullYear();
+	let calendar: Calendar | null = null;
 
 	async function handleSearch(year: number, name: string, nation: string, leagueId: string) {
 		competitions = await _loadCompetitions(year, name, nation, leagueId);
@@ -24,6 +29,33 @@
 		handleSearch(year, name, nation, leagueId);
 	}
 
+	$: {
+		updateCalendarEvents(competitions);
+	}
+
+	function handleToggleCalendar() {
+		if (!calendar) return;
+		viewCalendar ? calendar.destroy() : calendar.render();
+		viewCalendar = !viewCalendar;
+	}
+
+	function updateCalendarEvents(competitions: Competition[]) {
+		if (!calendar) return;
+		calendar.removeAllEvents();
+		competitions.forEach((c) => {
+			calendar?.addEvent({
+				title: c.name,
+				start: c.from,
+				end: c.to,
+				extendedProps: {
+					location: c.location,
+					nation: c.nation_code
+				}
+			});
+		});
+		if (competitions.length) calendar.gotoDate(competitions[0].from);
+	}
+
 	let timer: NodeJS.Timeout;
 	const debounce = (newValue: string) => {
 		clearTimeout(timer);
@@ -31,6 +63,25 @@
 			name = newValue;
 		}, 500);
 	};
+
+	onMount(() => {
+		let calendarEl: HTMLElement = document.getElementById('calendar')!;
+		calendar = new Calendar(calendarEl, {
+			plugins: [dayGridPlugin],
+			initialView: 'dayGridMonth',
+			displayEventTime: false,
+			eventClick: (info) => {
+				console.log(info.el);
+			},
+			eventContent: function (arg) {
+				let p1 = document.createElement('p');
+				p1.innerHTML = arg.event.title;
+				p1.style.overflow = 'clip';
+				let arrayOfDomNodes = [p1];
+				return { domNodes: arrayOfDomNodes };
+			}
+		});
+	});
 </script>
 
 <section id="competitions">
@@ -52,7 +103,7 @@
 			}}
 		>
 			{#each [...Array(10).keys()] as number}
-				<option>{currentYear - number}</option>
+				<option>{currentDate.getFullYear() - number}</option>
 			{/each}
 		</select>
 		<select bind:value={leagueId}>
@@ -68,10 +119,16 @@
 				<option value={n.id}>{n.code}</option>
 			{/each}
 		</select>
+		<input id="toggle-calendar" type="checkbox" on:click={() => handleToggleCalendar()} />
+		<label for="toggle-calendar">View Calendar</label>
 	</div>
-	<div class="grid grid-cols-1 lg:grid-cols-2 gap-5 my-10">
-		{#each competitions as competition, index (index)}
-			<EventCard {competition} />
-		{/each}
-	</div>
+	{#if !viewCalendar}
+		<div class="grid grid-cols-1 lg:grid-cols-2 gap-5 my-10">
+			{#each competitions as competition, index (index)}
+				<EventCard {competition} />
+			{/each}
+		</div>
+	{/if}
+
+	<div id="calendar" />
 </section>
