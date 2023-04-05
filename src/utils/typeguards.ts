afbc8a98c22864a487809450c7ec8381862958de
Dorian Zedler <dorian@itsblue.de>
2023-04-06 00:12:59 +0200

    Fix: competitions missing in calendar

--- src/utils/typeguards.ts
+++
@@ -1,7 +0,0 @@
-import type { Competition } from 'src/types/Competition';
-
-export function isPromise(
-	value: Competition[] | Promise<Competition[]>
-): value is Promise<Competition[]> {
-	return Object.prototype.toString.call(value) === '[object Promise]';
-}
