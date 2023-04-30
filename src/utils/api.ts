615f9c62d2db870b574086a6aaf6ee31a254ec41
Dorian Zedler <dorian@itsblue.de>
2023-04-30 09:57:03 +0200

    Fix: remove process env

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -16,8 +16,6 @@ export const fetchEndpoint = async <T>(
 	let headers: Record<string, string> = {};
 	if (platform?.env?.API_TOKEN) {
 		headers['Authorization'] = `Bearer ${platform.env.API_TOKEN}`;
-	} else if (process.env.API_TOKEN) {
-		headers['Authorization'] = `Bearer ${process.env.API_TOKEN}`;
 	}
 
 	const response = await fetch(`${API_URL}/${path}`, {
