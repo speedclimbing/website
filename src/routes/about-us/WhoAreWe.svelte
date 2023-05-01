16fdb5ec04eda741771f05170b6f19438ffe2a03
Dorian Zedler <dorian@itsblue.de>
2023-05-01 14:07:20 +0200

    Chore: align left

--- src/routes/about-us/WhoAreWe.svelte
+++ src/routes/about-us/WhoAreWe.svelte
@@ -8,7 +8,8 @@
 			name: 'Dorian Zedler',
 			roles: ['software architecture', 'development'],
 			about: `I'm a passionate speed climber and software developer. 
-                When I'm not climbing, I study computer science at the TU Munich and build software like this website. You may have heard of the 'Speedclimbing Stopwatch'-App - another project of mine.`,
+                When I'm not climbing, I study computer science at the TU Munich and build software like this website. 
+				You may have heard of the 'Speedclimbing Stopwatch'-App - another project of mine.`,
 			website: 'https://dorian.im',
 			image: '/images/about-us/dorian.jpg'
 		},
@@ -21,7 +22,9 @@
 		{
 			name: 'Peter Schnabel',
 			roles: ['consulting', 'statistics'],
-			about: `I'm the national coach of the German speedclimbing team and totally nuts about speed climbing. Even though I originally come from track and field, I've been trying to transfer my knowledge to climbing since 2021, and I'm getting a little bit better every day.`,
+			about: `I'm the national coach of the German speedclimbing team and totally nuts about speed climbing. 
+			Even though I originally come from track and field, I've been trying to transfer my knowledge to climbing since 2021, 
+			and I'm getting a little bit better every day.`,
 			image: '/images/about-us/peter.jpg'
 		}
 	];
@@ -39,14 +42,14 @@
 	<h2 class="text-4xl font-bold">Who we are</h2>
 	<Line color="red" />
 
-	<h4 class="text-2xl text-center font-bold pb-6">The core team</h4>
+	<h4 class="text-2xl font-bold pb-6">The core team</h4>
 
-	<p class="text-center text-lg pb-6">
+	<p class="text-lg pb-6">
 		We are a passionate team and dedicate our free time to the development of this website. We want
 		it to become a central palce of information for the whole speedclimbing community.
 	</p>
 
-	<div class="flex items-stretch flex-wrap justify-center gap-4">
+	<div class="flex items-stretch justify-center flex-wrap gap-4">
 		{#each people as person}
 			<Card img={person.image} href={person.website} horizontal class="flex-auto">
 				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -64,15 +67,15 @@
 		{/each}
 	</div>
 
-	<h4 class="text-2xl text-center font-bold pt-10 pb-6">Contributors</h4>
+	<h4 class="text-2xl font-bold pt-10 pb-6">Contributors</h4>
 
-	<p class="text-center text-lg pb-6">
+	<p class="text-lg pb-6">
 		This website is an open source project and everone is welcome to contribute to it on
 		<a href="https://github.com/speedclimbing/website" target="_blank">GitHub </a>! We'd like to
 		thank everyone who has done so and thereby helped the project to succeed.
 	</p>
 
-	<div class="flex items-stretch flex-wrap justify-center gap-4">
+	<div class="flex items-stretch flex-wrap gap-4">
 		{#each contributors as contributor}
 			<Badge
 				color="dark"
