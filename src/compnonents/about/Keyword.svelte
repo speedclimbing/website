774848baaa479571e5eb0f37e7ca52f011cecd95
Dorian Zedler <dorian@itsblue.de>
2023-05-01 14:12:03 +0200

    Chore: remove Keyword Component

--- src/compnonents/about/Keyword.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	export let color: string = 'red';
-	export let bold: boolean = true;
-	export let uppercase: boolean = false;
-</script>
-
-<span class="text-{color} {uppercase && 'uppercase'} {bold && 'font-bold'}"><slot /></span>
--- src/routes/about/TheRules.svelte
+++ src/routes/about/TheRules.svelte
@@ -1,7 +1,6 @@
 <script>
 	import { P } from 'flowbite-svelte';
 	import Line from 'compnonents/shared/Line.svelte';
-	import Keyword from 'compnonents/about/Keyword.svelte';
 </script>
 
 <section class="py-6">
@@ -10,26 +9,26 @@
 
 	<P
 		>A speedclimbing competition consists of two parts:
-		<Keyword color="blue-500">training</Keyword>,
-		<Keyword color="yellow">qualification</Keyword>
+		<span class="text-blue-500 font-bold">training</span>,
+		<span class="text-yellow font-bold">qualification</span>
 		and
-		<Keyword color="red">final</Keyword>.
+		<span class="text-red font-bold">final</span>.
 		<br />
 		<br />
-		In the <Keyword uppercase color="blue-500">training</Keyword>, the athletes have to runs which
-		have no meaning for the result. They are used to check out the wall and to get used to the
-		conditions.
+		In the <span class="text-blue-500 uppercase font-bold">training</span>, the athletes have to
+		runs which have no meaning for the result. They are used to check out the wall and to get used
+		to the conditions.
 		<br />
-		In the <Keyword uppercase color="yellow">qualification</Keyword>, all athletes climb the wall
-		twice, once on lane A and once on lane B. Only the better attempt is used for the result. The
-		best 16, 8 or 4 athletes will qualify for the final, depending on how many athletes compete. In
-		this stage,
+		In the <span class="text-yellow uppercase font-bold">qualification</span>, all athletes climb
+		the wall twice, once on lane A and once on lane B. Only the better attempt is used for the
+		result. The best 16, 8 or 4 athletes will qualify for the final, depending on how many athletes
+		compete. In this stage,
 		<b>it does not matter if the athletes win or lose the race, only the time counts</b>. False
 		starts lead to immideate disqualification, even when the athlete has already a valid result from
 		the previous run.
 		<br />
-		The <Keyword uppercase color="red">final</Keyword> consists of multiple elimination stages. This
-		is where it gets exciting! In the final, the time does not matter anymore, it's only about being
+		The <span class="text-red uppercase font-bold">final</span> consists of multiple elimination stages.
+		This is where it gets exciting! In the final, the time does not matter anymore, it's only about being
 		the first one at the top. In every round, the first place competes against the last place, the second
 		place competes against the second last place and so on. The winner of each round advances to the
 		next round. The loser is eliminated. When only four athletes are left, it's called the semi-final.
