772f90957df40d7a24d44b6fc7755c7f029682a0
Dorian Zedler <dorian@itsblue.de>
2023-05-01 10:45:26 +0200

    Chore: use tailwind

--- src/compnonents/shared/AnchorPoint.svelte
+++ src/compnonents/shared/AnchorPoint.svelte
@@ -2,14 +2,4 @@
 	export let id: string;
 </script>
 
-<a class="none anchor" {id} href="/">#</a>
-
-<style>
-	a.anchor {
-		display: block;
-		position: relative;
-		top: -100px;
-		visibility: hidden;
-		height: 0px;
-	}
-</style>
+<a class="block relative top-[-100px] invisible h-0" {id} href="/">#</a>
