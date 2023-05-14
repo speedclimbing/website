e47ac49e5b2abfb951759a9beac5032f7698f105
Dorian Zedler <dorian@itsblue.de>
2023-05-14 11:36:05 +0200

    Chore: remove unrelated code

--- src/stores.ts
+++ src/stores.ts
@@ -1,26 +1,3 @@
-import { browser } from '$app/environment';
 import { writable } from 'svelte/store';
 
-const prefersDarkModeStore = () => {
-	const dark = browser
-		? window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-		: false;
-
-	const { subscribe, set } = writable(dark);
-
-	if (browser) {
-		window
-			.matchMedia('(prefers-color-scheme: dark)')
-			.addEventListener('change', (event: MediaQueryListEvent) => {
-				set(event.matches);
-			});
-	}
-	console.log('subscribe!');
-
-	return {
-		subscribe
-	};
-};
-
-export const prefersDarkMode = prefersDarkModeStore();
 export const nextId = writable(0);
