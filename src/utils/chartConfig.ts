01a569ccefc5090f9aade16019858063d48f1f59
lamorak13 <j.e.mayerhofer@gmail.com>
2023-05-14 11:58:33 +0200

    Chore: refactor chartConfig

--- src/utils/chartConfig.ts
+++ src/utils/chartConfig.ts
@@ -5,26 +5,51 @@ const getOrCreateTooltip = (chart: Chart) => {
 
 	if (!tooltipEl) {
 		tooltipEl = document.createElement('div');
+		tooltipEl.classList.add('custom-tooltip');
 
-		tooltipEl.style.paddingTop = '10px';
-		tooltipEl.style.paddingBottom = '10px';
-		tooltipEl.style.paddingLeft = '20px';
-		tooltipEl.style.paddingRight = '20px';
-		tooltipEl.style.borderRadius = '6px';
-		tooltipEl.style.pointerEvents = 'none';
-		tooltipEl.style.position = 'absolute';
-		tooltipEl.style.transform = 'translate(-50%, 0)';
-		tooltipEl.style.transition = 'all .25s ease-in-out';
-		tooltipEl.style.marginTop = '15px';
-		tooltipEl.style.boxShadow = '0 10px 50px 0 rgba(17,24,39,.1)';
 		const table = document.createElement('table');
-		table.style.margin = '0px';
 
 		tooltipEl.appendChild(table);
 		chart.canvas.parentNode?.appendChild(tooltipEl);
 	}
 
 	return tooltipEl;
+};
+
+const setTooltipBody = (tooltip: TooltipModel<'line'>, tooltipEl: HTMLDivElement) => {
+	const titleLines = tooltip.title || [];
+	const bodyLines = tooltip.body.map((b) => b.lines);
+	const tableHead = document.createElement('thead');
+
+	titleLines.forEach((title) => {
+		const tr = document.createElement('tr');
+		const th = document.createElement('th');
+		const text = document.createTextNode(title);
+
+		th.appendChild(text);
+		tr.appendChild(th);
+		tableHead.appendChild(tr);
+	});
+
+	const tableBody = document.createElement('tbody');
+	bodyLines.forEach((body) => {
+		const tr = document.createElement('tr');
+		const td = document.createElement('td');
+		const text = document.createTextNode(body[0]);
+
+		td.appendChild(text);
+		tr.appendChild(td);
+		tableBody.appendChild(tr);
+	});
+
+	const tableRoot = tooltipEl.querySelector('table');
+
+	while (tableRoot?.firstChild) {
+		tableRoot.firstChild.remove();
+	}
+
+	tableRoot?.appendChild(tableHead);
+	tableRoot?.appendChild(tableBody);
 };
 
 const tooltipHandler = (context: { chart: Chart; tooltip: TooltipModel<'line'> }) => {
@@ -36,58 +61,21 @@ const tooltipHandler = (context: { chart: Chart; tooltip: TooltipModel<'line'> }
 		return;
 	}
 
-	if (tooltip.body) {
-		const titleLines = tooltip.title || [];
-		const bodyLines = tooltip.body.map((b) => b.lines);
-
-		const tableHead = document.createElement('thead');
-
-		titleLines.forEach((title) => {
-			const tr = document.createElement('tr');
-			tr.style.borderWidth = '0';
-
-			const th = document.createElement('th');
-			th.style.borderWidth = '0';
-			const text = document.createTextNode(title);
-
-			th.appendChild(text);
-			tr.appendChild(th);
-			tableHead.appendChild(tr);
-		});
-
-		const tableBody = document.createElement('tbody');
-		bodyLines.forEach((body, i) => {
-			const tr = document.createElement('tr');
-			tr.style.backgroundColor = 'inherit';
-			tr.style.borderWidth = '0';
-
-			const td = document.createElement('td');
-			td.style.borderWidth = '0';
-
-			const text = document.createTextNode(body[0]);
-
-			td.appendChild(text);
-			tr.appendChild(td);
-			tableBody.appendChild(tr);
-		});
-
-		const tableRoot = tooltipEl.querySelector('table');
-
-		while (tableRoot?.firstChild) {
-			tableRoot.firstChild.remove();
-		}
-
-		tableRoot?.appendChild(tableHead);
-		tableRoot?.appendChild(tableBody);
-	}
+	setTooltipBody(tooltip, tooltipEl);
 
 	const { offsetLeft: positionX, offsetTop: positionY } = chart.canvas;
 
 	tooltipEl.style.opacity = '1';
 	tooltipEl.style.left = positionX + tooltip.caretX + 'px';
 	tooltipEl.style.top = positionY + tooltip.caretY + 'px';
-	tooltipEl.style.background = chart.options.plugins!.tooltip!.backgroundColor!.toString();
-	tooltipEl.style.color = chart.options.plugins!.tooltip!.bodyColor!.toString();
+};
+
+const ticksColor = (darkMode: boolean) => {
+	return darkMode ? 'rgba(255, 255, 255, .6)' : 'rgba(17, 24, 39, 0.4)';
+};
+
+const lineColor = (darkMode: boolean) => {
+	return darkMode ? 'rgba(255, 255, 255, .1)' : 'rgba(17, 24, 39, 0.1)';
 };
 
 export const chartConfig = (
@@ -120,25 +108,22 @@ export const chartConfig = (
 				tooltip: {
 					position: 'nearest',
 					enabled: false,
-					backgroundColor: darkMode ? '#1F2937' : 'white',
-					bodyColor: darkMode ? 'white' : '#1F2937',
 					external: tooltipHandler
 				}
 			},
 			scales: {
 				y: {
-					stacked: true,
 					ticks: {
-						color: darkMode ? 'rgba(255, 255, 255, .6)' : 'rgba(17, 24, 39, 0.4)',
+						color: ticksColor(darkMode),
 						stepSize: 0.1
 					},
-					grid: { color: darkMode ? 'rgba(255, 255, 255, .1)' : 'rgba(17, 24, 39, 0.1)' },
-					border: { color: darkMode ? 'rgba(255, 255, 255, .1)' : 'rgba(17, 24, 39, 0.1)' }
+					grid: { color: lineColor(darkMode) },
+					border: { color: lineColor(darkMode) }
 				},
 				x: {
-					ticks: { color: darkMode ? 'rgba(255, 255, 255, .4)' : 'rgba(17, 24, 39, 0.4)' },
-					grid: { color: darkMode ? 'rgba(255, 255, 255, .1)' : 'rgba(17, 24, 39, 0.1)' },
-					border: { color: darkMode ? 'rgba(255, 255, 255, .1)' : 'rgba(17, 24, 39, 0.1)' }
+					ticks: { color: ticksColor(darkMode) },
+					grid: { color: lineColor(darkMode) },
+					border: { color: lineColor(darkMode) }
 				}
 			}
 		}
