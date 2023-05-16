6ba0e9e9e19edd91961dc0536343e4df6e269cfc
lamorak13 <j.e.mayerhofer@gmail.com>
2023-05-17 00:46:13 +0200

    Feat: add male/female switch and season select

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -3,31 +3,72 @@
 	import Hero from './Hero.svelte';
 	import SeasonStats from './SeasonStats.svelte';
 	import AllTimeStats from './AllTimeStats.svelte';
+	import SwitchButton from 'compnonents/shared/buttons/SwitchButton.svelte';
+	import SelectFilter from 'compnonents/shared/inputs/SelectFilter.svelte';
 
 	let bar: HTMLElement;
-	let season: number | undefined = undefined;
+	let season: number = new Date().getFullYear();
+	let showSeasonStats: boolean = true;
+
+	const options = [
+		{
+			year: 2023
+		},
+		{
+			year: 2022
+		},
+		{
+			year: 2021
+		},
+		{
+			year: 2020
+		}
+	];
 
 	const handleClickSeasonStats = () => {
-		bar.style.marginLeft = '0';
-		season = new Date().getFullYear();
+		showSeasonStats = true;
 	};
 
 	const handleClickAllTimeStats = () => {
-		bar.style.marginLeft = 'calc(100%/2)';
-		season = undefined;
+		showSeasonStats = false;
 	};
 </script>
 
 <Hero />
 <section class="bg-white dark:bg-black self ">
-	<div class="grid grid-cols-2  items-center justify-items-center text-xl py-4 dark:text-white">
-		<button class="cursor-pointer" on:click={handleClickSeasonStats}>Season Stats</button>
-		<button class="cursor-pointer" on:click={handleClickAllTimeStats}>All time Stats</button>
+	<div class="grid lg:grid-cols-3">
+		<div
+			class="grid grid-cols-2 lg:col-span-2 items-center justify-items-center text-xl dark:text-white py-5 relative"
+		>
+			<button class="cursor-pointer" on:click={handleClickSeasonStats}>Season Stats</button>
+			<button class="cursor-pointer" on:click={handleClickAllTimeStats}>All time Stats</button>
+			<div
+				class="bg-red rounded-sm w-[calc(100%/2)] h-1 transition-[left] absolute bottom-[-1px] {showSeasonStats
+					? 'left-0'
+					: 'left-[calc(100%/2)]'}"
+				bind:this={bar}
+			/>
+		</div>
+		<div class="flex justify-end gap-2 my-auto order-first lg:order-2 py-3">
+			{#if showSeasonStats}
+				<SelectFilter
+					bind:value={season}
+					{options}
+					valueProperty="year"
+					textProperty="year"
+					defaultText="Season"
+				/>
+			{/if}
+			<SwitchButton
+				rightClickAction={() => {}}
+				leftClickAction={() => {}}
+				leftString="Males"
+				rightString="Females"
+			/>
+		</div>
 	</div>
-
-	<div class="bg-red rounded-sm w-[calc(100%/2)] h-1 transition-[margin-left] " bind:this={bar} />
 </section>
-{#if season}
+{#if showSeasonStats}
 	<SeasonStats />
 	<About />
 {:else}
