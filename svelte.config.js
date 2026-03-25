02f5faa724311164834d4b7aa37bed0579daefa2
Dorian Zedler <dorian@itsblue.de>
2026-03-25 22:34:28 +0100

    Chore: upgrade dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,8 @@
 import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
-
 	kit: {
 		adapter: adapter(),
 		alias: {
@@ -18,7 +10,8 @@ const config = {
 			types: './src/types',
 			components: './src/components'
 		}
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
